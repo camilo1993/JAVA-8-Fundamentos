fc16533f9ee5a7a24ff6840f9d848f22558adb0f
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-11 18:59:28 -0300

    Variables

---
+++ Curso Java-Estructuras principales III/Variables/src/variables/Variables.java
@@ -0,0 +1,20 @@
+
+package variables;
+
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int edad=35;     
+        
+        
+               
+        
+        System.out.println(edad);
+        
+        edad=75;
+        
+        System.out.println(edad);
+    }
+    
+}
