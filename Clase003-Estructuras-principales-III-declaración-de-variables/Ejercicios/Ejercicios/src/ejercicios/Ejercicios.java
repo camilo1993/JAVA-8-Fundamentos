e0f06d5435ee04b1f9dd4f9de78ecc0c0d266715
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-12 16:26:33 -0300

    modificación, ejercicio

---
+++ Clase003-Estructuras-principales-III-declaración-de-variables/Ejercicios/Ejercicios/src/ejercicios/Ejercicios.java
@@ -0,0 +1,29 @@
+
+
+package ejercicios;
+
+
+public class Ejercicios {
+
+    public static void main(String[] args) {
+        String nombre;
+        
+        byte edad;
+        double salario;
+        boolean carnet;
+               
+        nombre="ana";
+        edad=27;
+        salario=1800.50;
+        carnet=true;
+        
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        System.out.println(carnet);
+               
+        
+        
+    }
+    
+}
