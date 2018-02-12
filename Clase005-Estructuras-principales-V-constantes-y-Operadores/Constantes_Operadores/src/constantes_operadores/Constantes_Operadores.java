8a37c51ab1ee6e11201eb36f7f54df258d370f3e
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-12 19:02:57 -0300

    Constantes Operadores

---
+++ Clase005-Estructuras-principales-V-constantes-y-Operadores/Constantes_Operadores/src/constantes_operadores/Constantes_Operadores.java
@@ -0,0 +1,25 @@
+
+package constantes_operadores;
+
+public class Constantes_Operadores {
+
+
+    public static void main(String[] args) {
+        
+        final double apulgadas=2.54;
+        
+        double cm=6;
+        
+        double resultado=cm/apulgadas;
+        
+        
+        System.out.println("En" + cm + "cm hay" + resultado + "pulgadas")
+        
+    
+        
+
+        
+        
+    }
+    
+}
