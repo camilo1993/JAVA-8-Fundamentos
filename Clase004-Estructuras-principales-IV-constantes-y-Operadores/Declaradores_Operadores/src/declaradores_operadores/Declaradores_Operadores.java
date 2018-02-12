7481de56b21cbb3d4d3af82d73f28a6242d063e2
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-12 18:37:51 -0300

    Declaradores Operadores

---
+++ Clase004-Estructuras-principales-IV-constantes-y-Operadores/Declaradores_Operadores/src/declaradores_operadores/Declaradores_Operadores.java
@@ -0,0 +1,24 @@
+
+package declaradores_operadores;
+
+public class Declaradores_Operadores {
+
+   
+    public static void main(String[] args) {
+       
+        
+       double a=5;
+       
+       double b;
+       
+       b=7;
+       
+       double c=b/a;
+       
+       //c+=6;
+       
+       //c-=6
+       System.out.println(c);
+    }
+    
+}
