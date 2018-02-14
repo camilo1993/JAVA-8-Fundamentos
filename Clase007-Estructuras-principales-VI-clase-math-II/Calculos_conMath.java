a642b1a126082ac5c912117f329f60bbdc33a9d2
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-14 19:43:46 -0300

    Calculos Math

---
+++ Clase007-Estructuras-principales-VI-clase-math-II/Calculos_conMath.java
@@ -0,0 +1,25 @@
+
+package calculos_conmath;
+
+public class Calculos_conMath {
+
+
+     
+    public static void main(String[] args) {
+
+    //double raíz=Math.sqrt(9);
+   
+    double base=5;
+    
+    double exponente=3;
+    
+    
+    int resultado=(int)Math.pow(base, exponente);
+           
+    
+    
+    
+System.out.println("El resultado de" + base + "elevado a" + exponente+ "es" + resultado);
+    }
+    
+}
