1533c5a2607d085b0aa261cea887ab42ba5b904a
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-02-21 16:29:48 -0300

    Api

---
+++ Clase010-Acercamiento-a-la-API-Paquetes/Prueba/src/prueba/Prueba.java
@@ -0,0 +1,18 @@
+package prueba;
+
+import java.util.*;
+
+
+public class Prueba {
+
+
+    public static void main(String[] args) {
+      
+        String nombre;
+        
+        Scanner miobjeto;
+                
+        Locale miobjeto2;
+    }
+    
+}
