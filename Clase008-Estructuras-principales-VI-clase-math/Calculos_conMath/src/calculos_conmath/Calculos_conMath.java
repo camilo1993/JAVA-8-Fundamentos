25b2ffa54230a0a26a02a6822efa03fcd3cc421f
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-16 23:59:53 -0300

    Estructuras principales VI clase math I

---
+++ Clase008-Estructuras-principales-VI-clase-math/Calculos_conMath/src/calculos_conmath/Calculos_conMath.java
@@ -0,0 +1,16 @@
+
+package calculos_conmath;
+
+public class Calculos_conMath {
+
+
+     
+    public static void main(String[] args) {
+
+
+double raíz=Math.sqrt(9);
+    
+System.out.println(raíz);
+    }
+    
+}
---
+++ Clase009-Estructuras-principales-VI-clase-math-I/Calculos_conMath/src/calculos_conmath/Calculos_conMath.java
@@ -0,0 +1,22 @@
+
+package calculos_conmath;
+
+public class Calculos_conMath {
+
+
+     
+    
+
+     
+    public static void main(String[] args) {
+
+
+double raíz=Math.sqrt(9);
+    
+System.out.println(raíz);
+    }
+    
+}
+    
+    
+
