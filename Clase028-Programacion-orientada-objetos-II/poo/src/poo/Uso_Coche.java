b1cfadd7aadd33ae0345d49217f0ee0734d7417e
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-26 17:03:07 -0300

    Add  POO II

---
+++ Clase028-Programacion-orientada-objetos-II/poo/src/poo/Uso_Coche.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author kamilo
+ */
+public class Uso_Coche {
+    
+    public static void main(String[] args) {
+        
+    Coche Renault=new Coche(); //INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE    
+        
+    System.out.println("Este coche tiene " + Renault.ruedas + "ruedas.");
+        
+        
+        
+        
+        
+    }
+    
+}
