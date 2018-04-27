03b9d02fee7d14a4e1d9add3b3a478bd5b60bd0e
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-27 16:46:47 -0300

    Add Arrays III

---
+++ Clase029-Programacion-orientada-objetos-III/poo/src/poo/Coche.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author kamilo
+ */
+    public class Coche {
+    
+   private int ruedas;
+    
+    int largo;
+    
+    int ancho;
+    
+    int motor;
+    
+    int peso;
+    
+    public Coche (){
+        
+        ruedas=4;
+        
+        largo=2000;
+        
+        ancho=300;
+        
+        motor=1600;
+        
+        peso=500;
+    }
+    
+}
---
+++ Clase029-Programacion-orientada-objetos-III/poo/src/poo/Uso_Coche.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author kamilo
+ */
+public class Uso_Coche {
+    
+    public static void main(String[] args) {
+
+        
+    Coche Renault=new Coche(); //INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE    
+        
+         Renault.ruedas=3
+        
+    
+    
+    System.out.println("Este coche tiene " + Renault.ruedas + "ruedas.");
+        
+        
+        
+        
+        
+    }
+    
+}
