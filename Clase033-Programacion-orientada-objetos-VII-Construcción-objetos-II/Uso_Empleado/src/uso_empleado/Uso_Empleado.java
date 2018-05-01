89c3f46e4e1323aa4753b42bf190a39c713136b0
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-04-30 22:24:26 -0300

    Add POO VII

---
+++ Clase033-Programacion-orientada-objetos-VII-Construcción-objetos-II/Uso_Empleado/src/uso_empleado/Uso_Empleado.java
@@ -0,0 +1,25 @@
+
+package uso_empleado;
+
+import java.util.Date;
+
+public class Uso_Empleado {
+
+    
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Empleado {
+
+    public Empleado(String nom, double sue, int agno, int mes, int dia) {
+
+    }
+    private String nombre;
+    
+    private double sueldo;
+    
+    private Date altaContrato;
+}
