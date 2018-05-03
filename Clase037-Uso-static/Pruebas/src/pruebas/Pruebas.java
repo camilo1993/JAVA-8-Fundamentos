182c9d225cde37b54fd5852c20d30d80f45bfcd2
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-05-02 22:05:49 -0300

    Add Uso static

---
+++ Clase037-Uso-static/Pruebas/src/pruebas/Pruebas.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author kamilo
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Empleados trabajador1 = new Empleados("Paco ");
+        
+        Empleados trabajador2 = new Empleados("Ana ");
+        
+        Empleados trabajador3 = new Empleados("Antonio");
+        
+        Empleados trabajador4 = new Empleados("Maria");
+        
+        trabajador1.cambiaSeccion("RRHH");
+  
+//        trabajador1.cambiaNombre("Maria");
+        System.out.println(trabajador1.devuelveDatos());
+
+        System.out.println(trabajador2.devuelveDatos());
+
+        System.out.println(trabajador3.devuelveDatos());
+        
+        System.out.println(trabajador4.devuelveDatos());
+
+    }
+
+}
+
+class Empleados {
+
+    public Empleados(String nom) {
+
+        nombre = nom;
+
+        seccion = "Administracion";
+        
+        Id=IdSiguiente;
+        
+        IdSiguiente++;
+        
+        
+       
+    }
+
+    public void cambiaSeccion(String seccion) {
+
+        this.seccion = seccion;
+
+    }
+
+//    public void cambiaNombre(String nombre) {
+//       
+//        this.nombre = nombre;
+//    }
+
+    public String devuelveDatos() {
+        return "El nombre es " + nombre + "y la seccion es " + seccion + " y el Id= "+ Id;
+    }
+
+    private final String nombre;
+
+    private String seccion;
+
+    private int Id;
+    
+    private static int IdSiguiente=1;
+}
