8454e740c27766c7926dfac5fa8aed5f9c892646
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-05-03 22:22:29 -0300

    Add Herencia I

---
+++ Clase040-Herencia-I/pooooooooo/JavaApplication60/src/poo/Furgoneta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author kamilo
+ */
+public class Furgoneta extends Coche {
+
+    private int capacidad_carga;
+
+    private int plaza_extra;
+
+    public Furgoneta(int plazas_extra, int capacidad_extra) {
+
+        super();    //llamar al constructor de la clase padre
+
+        this.capacidad_carga = capacidad_carga;
+
+        this.plaza_extra = plaza_extra;
+
+    }
+
+}
