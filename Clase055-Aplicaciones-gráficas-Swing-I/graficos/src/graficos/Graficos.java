037f6964e4b3bb77130b4dfb20fe167e8540f66a
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-05-15 15:55:14 -0400

    Add App gráficas Swing I

---
+++ Clase055-Aplicaciones-gráficas-Swing-I/graficos/src/graficos/Graficos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficos;
+
+/**
+ *
+ * @author kamilo
+ */
+
+import javax.swing.*;
+public class Graficos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        miMarco marco1=new miMarco();
+        
+        marco1.setVisible(true);
+ 
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    
+}
+
+class miMarco extends JFrame{
+    
+    public miMarco(){
+        
+        setSize(500, 300);
+        
+    }
+    
+    
+    
+}
