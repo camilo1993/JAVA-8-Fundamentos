fb4a7d15853c4e23a531faa7418fe83722b6e3e7
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-05-17 23:15:37 -0400

    Add App gráficas Swing VI

--- Clase060-Aplicaciones-gráficas-Swing-VI-Dibujando-en-el-Frame-II/Prueba_2d/src/prueba_2d/Prueba_2d.java
+++ Clase060-Aplicaciones-gráficas-Swing-VI-Dibujando-en-el-Frame-II/Prueba_2d/src/prueba_2d/Prueba_2d.java
@@ -5,6 +5,12 @@
  */
 package prueba_2d;
 
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.*;
+
 /**
  *
  * @author kamilo
@@ -15,7 +21,68 @@ public class Prueba_2d {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        MarcoConDibujos mimarco = new MarcoConDibujos();
+
+        mimarco.setVisible(true);
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         // TODO code application logic here
     }
-    
+
+}
+
+class MarcoConDibujos extends JFrame {
+
+    public MarcoConDibujos() {
+
+        setTitle("prueba de Dibujo");
+
+        setSize(400, 400);
+
+        LaminaConFiguras milamina = new LaminaConFiguras();
+
+        add(milamina);
+
+    }
+
+}
+
+class LaminaConFiguras extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+//        g.drawRect(50, 50, 200, 200);
+//        g.drawLine(100, 100, 300, 200);
+//        g.drawArc(50, 100, 100, 200, 120, 150);
+        Graphics2D g2 = (Graphics2D) g;
+
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
+
+        g2.draw(rectangulo);
+
+        Ellipse2D elipse = new Ellipse2D.Double();
+
+        elipse.setFrame(rectangulo);
+
+        g2.draw(elipse);
+
+        g2.draw(new Line2D.Double(100, 100, 300, 250));
+
+        double CentroenX = rectangulo.getCenterX();
+
+        double CentroenY = rectangulo.getCenterY();
+
+        double radio = 150;
+
+        Ellipse2D circulo = new Ellipse2D.Double();
+
+        circulo.setFrameFromCenter(CentroenX, CentroenY, CentroenX + radio, CentroenY + radio);
+
+        g2.draw(circulo);
+    }
+
 }
