a61d070c1116edc8250423702bc91fd3602388f6
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-06-12 05:49:41 -0400

    .

---
+++ Clase088-Componentes Swing.-Eventos-en-cuadros-de texto-I/cuadro_texto/src/cuadro_texto/Cuadro_texto.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadro_texto;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ *
+ * @author Kmilo
+ */
+public class Cuadro_texto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        MarcoPrueba mimarco = new MarcoPrueba();
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+// TODO code application logic here
+    }
+
+}
+
+class MarcoPrueba extends JFrame {
+
+    public MarcoPrueba() {
+
+        setBounds(500, 300, 500, 350);
+
+        LaminaPrueba milamina = new LaminaPrueba();
+
+        add(milamina);
+
+        setVisible(true);
+
+    }
+
+}
+
+class LaminaPrueba extends JPanel {
+
+    public LaminaPrueba() {
+
+        JTextField micampo = new JTextField(20);
+
+        add(micampo);
+    }
+    private class EscuchaTexto implements DocumentListener{
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+        
+        
+    }
+}
