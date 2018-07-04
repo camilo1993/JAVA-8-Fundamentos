a21c9513805bdc82475e571c7397366525002e04
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-07-04 04:06:06 -0400

    Add Práctica guiada V

--- Clase105-Componentes-Swing.-Creación-de-procesador-de-textos.-Práctica-guiada-V/Menu_procesador/src/menu_procesador/Menu_procesador.java
+++ Clase105-Componentes-Swing.-Creación-de-procesador-de-textos.-Práctica-guiada-V/Menu_procesador/src/menu_procesador/Menu_procesador.java
@@ -25,107 +25,104 @@ public class Menu_procesador {
     /**
      * @param args the command line arguments
      */
- public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		MarcoProcesador marco1=new MarcoProcesador();
-		marco1.setVisible(true);
-			
-		}
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-class MarcoProcesador extends JFrame{
-	
-	public MarcoProcesador(){
-		
-		setBounds(400,200,550,350);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Mi procesador de texto");
-		add(new LaminaProcesador());
-	}
+        MarcoProcesador marco1 = new MarcoProcesador();
+        marco1.setVisible(true);
+
+    }
 }
 
-class LaminaProcesador extends JPanel{
-	
-	private JTextPane areaTexto;
-	private JPanel laminaMenu;
-	private JMenuBar barraMenu;
-	private JMenu fuente, estilo, tamagno;
-	private Font letras;
-	
-	public LaminaProcesador(){
-		
-		setLayout(new BorderLayout());
-		laminaMenu = new JPanel();
-		add(laminaMenu, BorderLayout.NORTH);
-		areaTexto= new JTextPane();
-		areaTexto.setFont(new Font("Arial", Font.PLAIN, 20));
-		add(areaTexto, BorderLayout.CENTER);
-		
-		barraMenu = new JMenuBar();
-		laminaMenu.add(barraMenu);
-		
-		fuente = new JMenu("Fuente");
-		estilo = new JMenu("Estilo");
-		tamagno = new JMenu("Tamaño");
-		
-		barraMenu.add(fuente);
-		barraMenu.add(estilo);
-		barraMenu.add(tamagno);
-		
-		configuraMenu("Arial","fuente", 10);
-		configuraMenu("Courier","fuente", 10);
-		configuraMenu("Verdana","fuente", 10);
-		configuraMenu("Negrita","estilo", Font.BOLD);
-		configuraMenu("Cursiva","estilo", Font.ITALIC);
-		configuraMenu("12","tamagno", 12);
-		configuraMenu("16","tamagno", 16);
-		configuraMenu("20","tamagno", 20);
-		configuraMenu("24","tamagno", 24);
-		
-		letras = areaTexto.getFont();
-	}
-	
-	public void configuraMenu(String rotulo, String menu, int val){
-		
-		JMenuItem elem_menu = new JMenuItem(rotulo);
-		if(menu=="fuente"){
-			fuente.add(elem_menu);
-		}else if(menu=="estilo"){
-			estilo.add(elem_menu);
-		}else if(menu=="tamagno"){
-			tamagno.add(elem_menu);
-		}
+class MarcoProcesador extends JFrame {
 
-		elem_menu.addActionListener(new ActionListener(){
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-				if(menu=="fuente"){
-					
-					letras = new Font(rotulo, letras.getStyle(), letras.getSize());
-				}
-				else if (menu=="estilo"){
-					
-						if((letras.getStyle()+val)>Font.BOLD+Font.ITALIC || (letras.getStyle()==val)){
-							
-							letras=letras.deriveFont(letras.getStyle()-val);
-						}else{
-							
-							letras=letras.deriveFont(letras.getStyle()+val);
-						}
-					}
-				 
-				else if (menu=="tamagno"){ 
-					
-					float tam2 = val;
-					letras=letras.deriveFont(tam2);
-				}
-				
-				areaTexto.setFont(letras);
-			}
-		});
-	}
-} 
+    public MarcoProcesador() {
+
+        setBounds(400, 200, 550, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Mi procesador de texto");
+        add(new LaminaProcesador());
+    }
+}
+
+class LaminaProcesador extends JPanel {
+
+    private JTextPane areaTexto;
+    private JPanel laminaMenu;
+    private JMenuBar barraMenu;
+    private JMenu fuente, estilo, tamagno;
+    private Font letras;
+
+    public LaminaProcesador() {
+
+        setLayout(new BorderLayout());
+        laminaMenu = new JPanel();
+        add(laminaMenu, BorderLayout.NORTH);
+        areaTexto = new JTextPane();
+        areaTexto.setFont(new Font("Arial", Font.PLAIN, 20));
+        add(areaTexto, BorderLayout.CENTER);
+
+        barraMenu = new JMenuBar();
+        laminaMenu.add(barraMenu);
+
+        fuente = new JMenu("Fuente");
+        estilo = new JMenu("Estilo");
+        tamagno = new JMenu("Tamaño");
+
+        barraMenu.add(fuente);
+        barraMenu.add(estilo);
+        barraMenu.add(tamagno);
+
+        configuraMenu("Arial", "fuente", 10);
+        configuraMenu("Courier", "fuente", 10);
+        configuraMenu("Verdana", "fuente", 10);
+        configuraMenu("Negrita", "estilo", Font.BOLD);
+        configuraMenu("Cursiva", "estilo", Font.ITALIC);
+        configuraMenu("12", "tamagno", 12);
+        configuraMenu("16", "tamagno", 16);
+        configuraMenu("20", "tamagno", 20);
+        configuraMenu("24", "tamagno", 24);
+
+        letras = areaTexto.getFont();
+    }
+
+    public void configuraMenu(String rotulo, String menu, int val) {
+
+        JMenuItem elem_menu = new JMenuItem(rotulo);
+        if (menu == "fuente") {
+            fuente.add(elem_menu);
+        } else if (menu == "estilo") {
+            estilo.add(elem_menu);
+        } else if (menu == "tamagno") {
+            tamagno.add(elem_menu);
+        }
+
+        elem_menu.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+
+                if (menu == "fuente") {
+
+                    letras = new Font(rotulo, letras.getStyle(), letras.getSize());
+                } else if (menu == "estilo") {
+
+                    if ((letras.getStyle() + val) > Font.BOLD + Font.ITALIC || (letras.getStyle() == val)) {
+
+                        letras = letras.deriveFont(letras.getStyle() - val);
+                    } else {
+
+                        letras = letras.deriveFont(letras.getStyle() + val);
+                    }
+                } else if (menu == "tamagno") {
+
+                    float tam2 = val;
+                    letras = letras.deriveFont(tam2);
+                }
+
+                areaTexto.setFont(letras);
+            }
+        });
+    }
+}
