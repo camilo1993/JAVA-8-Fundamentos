031106824a19a5d814b3f3ba82e7300fc46c914c
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-07-17 17:22:48 -0400

    Add Cuadros diálogo I

---
+++ Clase121-Aplicaciones-gráficas.-Ventanas-emergentes.-Cuadros-diálogo-I/Tipos_Cuadros_Dialogos/src/tipos_cuadros_dialogos/Tipos_Cuadros_Dialogos.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos_cuadros_dialogos;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class Tipos_Cuadros_Dialogos {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Cuadros_Dialogos mimarco = new Cuadros_Dialogos();
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        mimarco.setVisible(true);
+
+    }
+
+}
+
+class Cuadros_Dialogos extends JFrame {
+
+    public Cuadros_Dialogos() {
+
+        setBounds(500, 300, 400, 250);
+
+        add(new Lamina_Cuadros_Dialogos());
+
+    }
+
+}
+
+class Lamina_Cuadros_Dialogos extends JPanel {
+
+    public Lamina_Cuadros_Dialogos() {
+
+        boton1 = new JButton("boton 1");
+
+        boton2 = new JButton("boton2");
+
+        boton3 = new JButton("boton3");
+
+        boton4 = new JButton("boton4");
+
+        boton1.addActionListener(new Accion_botones());
+        boton2.addActionListener(new Accion_botones());
+        boton3.addActionListener(new Accion_botones());
+        boton4.addActionListener(new Accion_botones());
+
+        add(boton1);
+
+        add(boton2);
+
+        add(boton3);
+
+        add(boton4);
+
+    }
+
+    private class Accion_botones implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+
+            if (e.getSource() == boton1) {
+
+//				System.out.println("Has pulsado el botón 1");
+//			JOptionPane.showMessageDialog(Lamina_Cuadros_Dialogos.this, "Mensaje de prueba");
+                JOptionPane.showMessageDialog(Lamina_Cuadros_Dialogos.this, "Mensaje de Prueba", "Advertencia", 2);
+
+            } else if (e.getSource() == boton2) {
+
+//				System.out.println("Has pulsado el botón 2");
+                JOptionPane.showInputDialog(Lamina_Cuadros_Dialogos.this, "Introduce Nombre", "Introduce Datos", 2);
+
+            } else if (e.getSource() == boton3) {
+
+                System.out.println("Has pulsado el botón 3");
+
+            } else {
+
+                System.out.println("Has pulsado el botón 4");
+
+            }
+
+        }
+
+    }
+
+    private JButton boton1, boton2, boton3, boton4;
+}
