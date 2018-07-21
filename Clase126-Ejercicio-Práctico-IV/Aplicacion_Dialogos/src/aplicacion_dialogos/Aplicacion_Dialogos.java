b2db4d376cadeb3e6b9755710e5baf396fcf1ad9
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-07-21 16:10:53 -0400

    Add Ejercicio Práctico IV

---
+++ Clase126-Ejercicio-Práctico-IV/Aplicacion_Dialogos/src/aplicacion_dialogos/Aplicacion_Dialogos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion_dialogos;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Kmilo
+ */
+public class Aplicacion_Dialogos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        Marco_Dialogos mimarco = new Marco_Dialogos();
+        
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        mimarco.setVisible(true);
+        
+    }
+    
+}
