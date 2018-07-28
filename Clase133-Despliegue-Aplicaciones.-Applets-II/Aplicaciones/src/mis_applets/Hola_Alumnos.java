2e593a40fa68c19b819cc0d5e30e36cde9f82c80
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-07-28 17:18:16 -0400

    Add Applets II

---
+++ Clase133-Despliegue-Aplicaciones.-Applets-II/Aplicaciones/src/mis_applets/Hola_Alumnos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mis_applets;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Kmilo
+ */
+public class Hola_Alumnos extends JApplet {
+
+    public void init() {
+
+        JLabel rotulo = new JLabel("Hola alumnos", SwingConstants.CENTER);
+
+        add(rotulo);
+
+    }
+
+}
