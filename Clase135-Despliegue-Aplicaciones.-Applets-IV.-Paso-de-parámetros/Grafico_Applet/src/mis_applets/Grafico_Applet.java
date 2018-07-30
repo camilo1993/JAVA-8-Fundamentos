08130af6116d5ea62cb54aeccd2698f7152a1aae
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-07-30 13:36:10 -0400

    Add Applets IV

--- Clase135-Despliegue-Aplicaciones.-Applets-IV.-Paso-de-parámetros/Grafico_Applet/src/mis_applets/Grafico_Applet.java
+++ Clase135-Despliegue-Aplicaciones.-Applets-IV.-Paso-de-parámetros/Grafico_Applet/src/mis_applets/Grafico_Applet.java
@@ -13,8 +13,14 @@ import java.awt.geom.*;
 public class Grafico_Applet extends JApplet {
 
     public void init() {
+        
+        int numero1 = Integer.parseInt(getParameter("barra.1"));
 
-        LaminaGrafico milamina = new LaminaGrafico();
+        int numero2 = Integer.parseInt(getParameter("barra.2"));
+
+        int numero3 = Integer.parseInt(getParameter("barra.3"));
+
+        LaminaGrafico milamina = new LaminaGrafico(numero1,numero2,numero3);
 
         add(milamina);
 
@@ -23,6 +29,15 @@ public class Grafico_Applet extends JApplet {
 }
 
 class LaminaGrafico extends JPanel {
+    
+    public LaminaGrafico (int a,int b,int c){
+        
+        
+        g1=a;
+        gr2=b;
+        g3=c;
+        
+    }
 
     public void paint(Graphics g) {
 
@@ -32,11 +47,11 @@ class LaminaGrafico extends JPanel {
 
         g2.rotate(180 / 180.0 * Math.PI, 125, 100);
 
-        Rectangle2D graf1 = new Rectangle2D.Double(50, 0, 50, 50);
+        Rectangle2D graf1 = new Rectangle2D.Double(50, 0, 50, g1);
 
-        Rectangle2D graf2 = new Rectangle2D.Double(100, 0, 50, 175);
+        Rectangle2D graf2 = new Rectangle2D.Double(100, 0, 50, gr2);
 
-        Rectangle2D graf3 = new Rectangle2D.Double(150, 0, 50, 100);
+        Rectangle2D graf3 = new Rectangle2D.Double(150, 0, 50, g3);
 
         g2.setPaint(Color.RED);
 
@@ -52,4 +67,6 @@ class LaminaGrafico extends JPanel {
 
     }
 
+    private int g1,gr2,g3;
+    
 }
