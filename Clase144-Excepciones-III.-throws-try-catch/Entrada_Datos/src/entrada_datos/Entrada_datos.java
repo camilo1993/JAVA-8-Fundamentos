78c8e54eda23ff40fb309e7687d4de4f8f7f81da
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-08-07 18:33:10 -0400

    Add Excepciones III

---
+++ Clase144-Excepciones-III.-throws-try-catch/Entrada_Datos/src/entrada_datos/Entrada_datos.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entrada_datos;
+
+import java.util.*;
+
+/**
+ *
+ * @author Kmilo
+ */
+public class Entrada_datos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("¿Qué deseas hacer?");
+		System.out.println("1. Introducir datos");
+		System.out.println("2. Salir del programa");
+		
+		Scanner entrada=new Scanner (System.in);
+		
+		int decision=entrada.nextInt();
+		
+		if (decision==1){
+			
+			pedirDatos();
+			
+		}else{
+			
+			System.out.println("Adios");
+			
+			System.exit(0);
+		}
+		
+		entrada.close();
+		
+	}
+	
+		static void pedirDatos() throws InputMismatchException  {			
+		
+		try {
+                    
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Introduce tu nombre, por favor");
+		
+		String nombre_usuario=entrada.nextLine();
+		
+		System.out.println("Introduce edad, por favor");
+		
+		int edad=entrada.nextInt();	
+			
+		System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad+1) + " años");
+		
+		entrada.close();
+                }catch(InputMismatchException e) {
+                    
+                    System.out.println("Que has introducido en la edad");
+                    
+                }
+		
+		System.out.println("Hemos terminado");
+		
+	}
+		
+		
+}
