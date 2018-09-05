38270aab6dc63d69408baee6a98d31ee317d16eb
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-09-05 18:51:32 -0300

    Add Clase File II

--- Clase160-Manipulación-archivos-y-directorios.-Clase-File-II/Creando/src/creando/NewClass.java
+++ Clase160-Manipulación-archivos-y-directorios.-Clase-File-II/Creando/src/creando/NewClass.java
@@ -5,10 +5,20 @@
  */
 package creando;
 
+
+import java.io.*;
+
 /**
  *
  * @author Kmilo
  */
 public class NewClass {
-    
+
+    public static void main(String[] args) {
+
+        File ruta = new File("E:" + File.separator + "GIT" + File.separator + "JAVA-8-Fundamentos" + File.separator + "Clase160-Manipulación-archivos-y-directorios.-Clase-File-II" + File.separator + "nuevo_directorio.txt");
+
+        ruta.delete();
+        
+    }
 }
