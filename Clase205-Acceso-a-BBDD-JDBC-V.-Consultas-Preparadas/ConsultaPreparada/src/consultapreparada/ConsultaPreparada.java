26bc29713b13f6ee2096f445d7e66b307c597d1c
camilo1993 <35336281+camilo1993@users.noreply.github.com>
2018-11-01 20:34:43 -0300

    Update Clase205-Acceso-a-BBDD-JDBC-V.-Consultas-Preparadas/ConsultaPreparada/src/consultapreparada/ConsultaPreparada.java

--- Clase205-Acceso-a-BBDD-JDBC-V.-Consultas-Preparadas/ConsultaPreparada/src/consultapreparada/ConsultaPreparada.java
+++ Clase205-Acceso-a-BBDD-JDBC-V.-Consultas-Preparadas/ConsultaPreparada/src/consultapreparada/ConsultaPreparada.java
@@ -21,47 +21,43 @@ public class ConsultaPreparada {
     public static void main(String[] args) {
         try {
             // TODO code application logic here
-            
+
             //1 CREAR CONEXION
             Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3308/pruebas", "root", "");
 
             //2 PREPARAR CONSULTA
-            PreparedStatement miSentencia = miConexion.prepareStatement("SELECT NOMBREARTÍCULO, SECCIÓN, PAÍSDEORIGEN FROM PRODUCTOS" + 
-                    " WHERE SECCIÓN=? AND PAÍSDEORIGEN=?");
+            PreparedStatement miSentencia = miConexion.prepareStatement("SELECT NOMBREARTÍCULO, SECCIÓN, PAÍSDEORIGEN FROM PRODUCTOS"
+                    + " WHERE SECCIÓN=? AND PAÍSDEORIGEN=?");
 
             //3 ESTABLECER PARAMETROS DE CONSULTA
-           
             miSentencia.setString(1, "deportes");
-            
+
             miSentencia.setString(2, "USA");
-            
+
             //4 EJECUTAR Y RECORRER CONSULTA
-            
-            ResultSet rs= miSentencia.executeQuery();
-            
+            ResultSet rs = miSentencia.executeQuery();
+
             while (rs.next()) {
-                
+
                 System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
             }
 
             rs.close();
-            
+
             //REUTILIZACION DE CONSULTA
-            
             System.out.println("Ejecucion segunda consulta");
 
             System.out.println("");
-            
+
             miSentencia.setString(1, "cerámica");
-            
+
             miSentencia.setString(2, "China");
-            
+
             //4 EJECUTAR Y RECORRER CONSULTA
-            
-            rs= miSentencia.executeQuery();
-            
+            rs = miSentencia.executeQuery();
+
             while (rs.next()) {
-                
+
                 System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
             }
 
